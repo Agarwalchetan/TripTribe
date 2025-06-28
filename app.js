21b2a0b5f14e25c51cc5044220e168a2dbd2eec1
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-06-28 15:22:48 +0530

    Solving Async Errors with wrapAsync functions

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-const ejsMate =  require("ejs-mate");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/TripTribe";
 
 main()
@@ -25,7 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 //Main Route
 app.get("/", (req, res) => {
@@ -51,12 +52,15 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  let listing = req.body.listing;
-  const newListing = new Listing(listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let listing = req.body.listing;
+    const newListing = new Listing(listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -66,14 +70,14 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update Route
-app.put("/listings/:id", async (req,res) => {
+app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id,{...req.body.listing} );
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
 });
 
 //Delete Route
-app.delete("/listings/:id", async (req,res) => {
+app.delete("/listings/:id", async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
@@ -94,7 +98,8 @@ app.delete("/listings/:id", async (req,res) => {
 //   res.send("successful testing");
 // });
 
-app.use((err ,req ,res ,next) => {
+// Error Handling Middleware
+app.use((err, req, res, next) => {
   res.send("something went wrong");
 });
 

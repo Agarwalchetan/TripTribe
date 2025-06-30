91a7cdb449fa3b020f763630181cc9cfc298f2b0
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-06-30 18:37:05 +0530

    Restructure Reviews Routes

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./utils/schema.js");
 
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/TripTribe";
 
@@ -40,52 +42,14 @@ app.get("/", (req, res) => {
 });
 
 
-
-//Validation for Schema(Middleware) "Review"
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
+//Listings
 app.use("/listings" , listings);
 
 //Reviews
-//Post Review Route
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-
-//Delete  Review Route
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-  })
-);
+app.use("/listings/:id/reviews" , reviews);
 
 
-// //Page Not Found
+//Page Not Found
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
@@ -94,7 +58,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong" } = err;
   // if (!err.message) err.message = "Something went wrong!";
-  res.status(statusCode).render("error.ejs", { message });
+  res.status(statusCode).render("error.ejs", { err });
 });
 
 app.listen(port, () => {

3429477039765de2e62daba05f8a4b1474a655b2
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-06-30 21:32:55 +0530

    Error Ejs Error Handling

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong" } = err;
   // if (!err.message) err.message = "Something went wrong!";
-  res.status(statusCode).render("error.ejs", { err });
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(port, () => {

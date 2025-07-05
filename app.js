6a34cb0934073e590d2ffb30046c8613b4cc51d0
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-07-05 23:43:24 +0530

    Using Mongo Atlas

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,9 +22,8 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const extraRouter = require("./routes/extra.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/TripTribe";
-
-
+// const MONGO_URL = "mongodb://127.0.0.1:27017/TripTribe";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -34,7 +34,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -45,7 +45,20 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 // Declaring Session Options
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: "brooklyn",
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
+  store,
   secret: "brooklyn",
   resave: false,
   saveUninitialized: true,

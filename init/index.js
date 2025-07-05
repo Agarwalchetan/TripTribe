1eca9952df845a7dc34e377f6f78e2fef2dfa0bf
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-07-06 00:26:17 +0530

    Seedec data

--- init/index.js
+++ init/index.js
@@ -1,29 +1,33 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const User = require("../models/user.js");
+require("dotenv").config({ path: "../.env" });
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/TripTribe";
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
+  console.log("Connected to DB");
 }
+
+main().catch((err) => console.error("DB connection error:", err));
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
+  await User.deleteMany({});
+
+  const user = new User({ email: "admin@example.com", username: "admin" });
+  const registeredUser = await User.register(user, "admin"); // passport-local-mongoose will hash password
+
+  const listingsWithOwner = initData.data.map((obj) => ({
     ...obj,
-    owner: "6864e7ea96b1020b28b6b929",
+    owner: registeredUser._id,
   }));
-  await Listing.insertMany(initData.data); //to access key data in Object initData
-  console.log("data was initialized");
+
+  await Listing.insertMany(listingsWithOwner);
+  console.log("Database seeded successfully");
+  mongoose.connection.close();
 };
 
 initDB();
